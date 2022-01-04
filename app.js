a2aaa1995d9eadb738803b885e76806ca8a9f396
arcteggzz <arcteggzz@gmail.com>
2022-01-05 00:39:15 +0100

    Structured the Ojects and Array elements in the code
    Added the counter function.
    Created the Player object
    Created the function to control the update of the Bard
    Created the function to get the player's move

--- app.js
+++ app.js
@@ -1,44 +1,51 @@
-//Ignore all the code below this line _________
-function playRound(){
-    let counter = -1
-    counter = updateCounter(counter)
-    updateGameBoard(counter)    
+
+let counter = 0
+//Selects and creates the array from the divs
+const squares = Array.from(document.querySelectorAll(".gameBoard div")) 
+squares.forEach(square => square.addEventListener("click", getPlayerMove))
+
+//Gameboard Array of Objects
+const gameBoardArray = [
+    {id : 1, dataValue : ""},
+    {id : 2, dataValue : ""},
+    {id : 3, dataValue : ""},
+    {id : 4, dataValue : ""},
+    {id : 5, dataValue : ""},
+    {id : 6, dataValue : ""},
+    {id : 7, dataValue : ""},
+    {id : 8, dataValue : ""},
+    {id : 9, dataValue : ""}
+]
+
+//Create Player Objects
+let playerOne = new Player("Tega", "X")
+let playerTwo = new Player("Jite", "O")
+
+//Class constructor for the player objects
+class Player{
+	constructor(name, marker){
+		this.name = name 
+        this.marker = "marker"
+	}
 }
 
-function updateGameBoard(counter){
-    if ((counter % 2 === 0) && (this.textContent === "")){
-        this.textContent = "O"
-    } else if((counter % 2 != 0) && (this.textContent ==="")){
-        this.textContent = "X"
+function getPlayerMove(){
+    counter = updateCounter()
+    for (const obj of gameBoardArray) {
+        //the first conditional matches the clicked div square to the right object in the array.
+        //the second conditional validates the user's move and prevents a user from clicking on a non-empty square.
+        if ((obj.id == this.dataset.position) && (this.textContent == "")){
+            obj.dataValue = 'X';     
+            break;
+        }
     }
+    updateGameBoard()
+}
+
+function updateGameBoard(){
+    squares.map(square => square.textContent = gameBoardArray[square.dataset.position - 1].dataValue)
 }
 
 function updateCounter(counter){
     return counter++
 }
-//Ignore all the code above this line _________
-
-//Gameboard Object
-const gameBoard = {
-    squareOne : "X",
-    squareTwo : "O",
-    sqareThree : "",
-    squareFour : "",
-    squareFive : "O",
-    squareSix : "X",
-    squareSeven : "",
-    squareEight : "X",
-    squareNine : "O"
-}
-
-//Selects and creates the array from the divs
-const squares = Array.from(document.querySelectorAll(".gameBoard div")) 
-
-//My attempt at solving the problem
-// squares.map(square => {
-//     //get the position of the div in the array
-//     let position = square.dataset.position
-//     //get the position of the correct gameBoard position
-//     //match the value of the object to the array div
-//     console.log(square, position)
-//     })

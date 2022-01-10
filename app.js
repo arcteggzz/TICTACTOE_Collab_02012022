0d4206796c8530c058456090f2be62e0fccb1df5
arcteggzz <arcteggzz@gmail.com>
2022-01-10 08:12:41 +0100

    Created the Development branch
    Tried to refactor the code block to make it neater, however code does not run properly right now.
    
    I suspected it is because of scope and global variables.
    
    Added comments to the code to make it easy for Ann to understand.

--- app.js
+++ app.js
@@ -1,37 +1,56 @@
+//This is the refactored Code. The Idea here was to make my code neater and reduce the scope of Variables.
+//We were just taught function scope and Global Scope in the class before they gave us this project. 
+//So I am trying to introduce this to my code.
+
+//This playerFactory is like the class used to create the player objects.
 const playerFactory = (name, marker) => {
     return {name, marker}
 }
 
+
 let counter = 0
-//Selects and creates the array from the divs
-const squares = Array.from(document.querySelectorAll(".gameBoard div")) 
-squares.forEach(square => square.addEventListener("click", getPlayerMove))
-
-//Gameboard Array of Objects
-const gameBoardArray = [
-    {id : 1, dataValue : ""},
-    {id : 2, dataValue : ""},
-    {id : 3, dataValue : ""},
-    {id : 4, dataValue : ""},
-    {id : 5, dataValue : ""},
-    {id : 6, dataValue : ""},
-    {id : 7, dataValue : ""},
-    {id : 8, dataValue : ""},
-    {id : 9, dataValue : ""}
-]
-
-//Create Player Objects uisng the Factory function method
-const playerOne = playerFactory("Tega", "X")
-const playerTwo = playerFactory("Jite", "O")
+const startButton = document.querySelector(".content button")
+startButton.addEventListener("click", startGame)
 
 
-function getPlayerMove(){
+function startGame(){
+    playOneRound(counter)
+}
+
+function playOneRound(counter){
+    //Create an array of Objects to represent the Gameboard.
+    //Each object of the array represents the information for one of the squares.
+    const gameBoardArray = [
+        {id : 1, dataValue : ""},
+        {id : 2, dataValue : ""},
+        {id : 3, dataValue : ""},
+        {id : 4, dataValue : ""},
+        {id : 5, dataValue : ""},
+        {id : 6, dataValue : ""},
+        {id : 7, dataValue : ""},
+        {id : 8, dataValue : ""},
+        {id : 9, dataValue : ""}
+    ]
+
+    //Create Player Objects uisng the Factory function method
+    const playerOne = playerFactory("Tega", "X")//Just a random input for player name.
+    const playerTwo = playerFactory("Jite", "O")//Just a random input for player name.
+
+    //Selects and creates the array from the divs
+    const squares = Array.from(document.querySelectorAll(".gameBoard div")) 
+    squares.forEach(square => square.addEventListener("click", ()=>
+        getPlayerMove(gameBoardArray, playerOne, playerTwo, square, squares, counter)))
+    }
+
+function getPlayerMove(gameBoardArray, playerOne, playerTwo, square, squares, counter){
+    //Step one in this function is to get the player move(i.e the clicked square)
+    //After getting the player move, we use it to update the game board array.
     for (const obj of gameBoardArray) {
         //the first conditional matches the clicked div square to the right object in the array.
         //the second conditional validates the user's move and prevents a user from clicking on a non-empty square.
-        if ((obj.id == this.dataset.position) && (this.textContent == "")){
+        if ((obj.id == square.dataset.position) && (square.textContent == "")){
             //only increment counter if move is valid
-            counter = updateCounter()
+            counter = updateCounter(counter)
             if (counter % 2 === 1) {
                 //updates the value of the object in the gameBoardArray
                 obj.dataValue = playerOne.marker
@@ -39,19 +58,25 @@ function getPlayerMove(){
                 //updates the value of the object in the gameBoardArray
                 obj.dataValue = playerTwo.marker
             }     
-            break;
+            return counter;
         }
     }
-    //update the board based on the updated gameBoardArray
-    updateGameBoard()
+    //Step3 is where we now display the game board on screen using the updated game board Array from step2.
+    //This step 3 is done by the function(updateGameBoard)
+    updateGameBoard(squares, gameBoardArray)
     //create a checker function here
 }
 
-function updateGameBoard(){
+
+function updateCounter(counter){
+    return counter = counter + 1
+}
+
+function updateGameBoard(squares, gameBoardArray){
+    //We map over each square and change it's text content to match the correct value.
+    //the function runs properly in the initial version. so I don't think there is an error here.
     squares.map(square => square.textContent = gameBoardArray[square.dataset.position - 1].dataValue)
 }
 
-function updateCounter(){
-    return counter = counter + 1
-}
+
 
